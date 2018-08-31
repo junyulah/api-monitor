3d00cd4c68a2b5f674cd1bd5de01da1226dca4fd
loveKino <842914439@qq.com>
2018-08-31 14:10:00 +0800

    example

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # api-monitor
+
 api monitor
---
+++ example/api-monitor.config.js
@@ -0,0 +1,35 @@
+const log = console.log; // eslint-disable-line
+
+module.exports = {
+  apis: [{
+    api: {
+      options: {
+        host: 'some-fake-domain.com',
+        port: '80',
+        path: '/api/fake',
+        headers: {
+          cookie: 'a=123'
+        }
+      }
+    },
+
+    reporter: {
+      type: 'http',
+      cnt: (errMsg, monitorObj) => {
+        return {
+          options: {
+            host: 'faketest.com',
+            port: 80,
+            path: '/api/report',
+            headers: {
+              'cache-control': 'no-cache',
+            }
+          },
+          data: `${errMsg}, ${JSON.stringify(monitorObj)}.`
+        }
+      }
+    },
+
+    period: 60 * 1000 // 60s
+  }]
+};
